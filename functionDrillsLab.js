db069c65793fb78f7726e3e6c23c93a074b34362
LucassNogueira <simplylucasjason@gmail.com>
2022-01-10 16:07:44 -0600

    problem 16 done

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -58,10 +58,9 @@ printName("Cameron");
 
 function greeting(name) {
   nameString = String(name);
-  return `Hello, ${nameString}`;
+  console.log(`Hello, ${nameString}`);
 }
-const greetingText = greeting("Jake");
-console.log(greetingText);
+greeting("jake");
 
 ////////////////// PROBLEM 5 ////////////////////
 
@@ -286,13 +285,21 @@ let contestants = [
   "Clove",
   "Marvel",
 ];
-let loser = "Glimmer";
+let loser = "Marvel";
 /*
   Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
   The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
 */
 
 //CODE HERE
+function theEliminator(contestants, loser) {
+  for (let i = 0; i < contestants.length; i++) {
+    if (contestants[i] === loser) {
+      return (newContestants = contestants.splice(i, 1));
+    }
+  }
+}
+console.log(theEliminator(contestants, loser));
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo.";
